fb9d9d4b5ecf1546932cc35482dd7b4d6b15fe46
Tayyab Bin Tahir <tahirt@DAT2-IM27.tss.oregonstate.edu>
2023-07-06 16:24:58 -0700

    single quoted string formatting

--- api-call.py
+++ api-call.py
@@ -12,7 +12,7 @@ def load_from_config():
     :returns: The config object
     """
 
-    with open("configuration.yaml", "r") as file:
+    with open('configuration.yaml', 'r') as file:
         return yaml.safe_load(file)
 
 
@@ -29,8 +29,8 @@ def log_error(error_message):
     formatted_message = f"[{current_time}] {error_message}"
 
     # Append the error message to the logfile
-    with open("logfile.txt", "a") as file:
-        file.write(formatted_message + "\n")
+    with open('logfile.txt', 'a') as file:
+        file.write(formatted_message + '\n')
 
 
 def get_access_token():
@@ -41,11 +41,11 @@ def get_access_token():
 
     # Load configurations from the YAML file
     config = load_from_config()
-    url = config["accessToken"]["url"]
-    enhancer = config["accessToken"]["enhancer"]
-    payload = config["accessToken"]["payload"]
-    headers = config["accessToken"]["headers"]
-    http_failed = config["httpResponses"]["failed"]
+    url = config['accessToken']['url']
+    enhancer = config['accessToken']['enhancer']
+    payload = config['accessToken']['payload']
+    headers = config['accessToken']['headers']
+    http_failed = config['httpResponses']['failed']
 
     response = requests.post(url,
                              data=payload,
@@ -54,7 +54,7 @@ def get_access_token():
 
     if response.status_code == 200:
         # Request was successful
-        access_token = response.json()["access_token"]
+        access_token = response.json()['access_token']
         if access_token:
             access_token = enhancer + access_token
         return access_token
@@ -99,8 +99,8 @@ def show_tasks():
 
     config = load_from_config()
     # Get the API options from the config
-    api_options = config["apiOptions"]
-    print(config["generalStrings"]["whichApi"])
+    api_options = config['apiOptions']
+    print(config['generalStrings']['whichApi'])
     for i, option in enumerate(api_options, start=1):
         print(f"{i}. {option}")
 
@@ -112,7 +112,7 @@ def get_user_choice():
     """
 
     config = load_from_config()
-    return input(config["generalStrings"]["enterChoice"])
+    return input(config['generalStrings']['enterChoice'])
 
 
 def get_url(choice):
@@ -123,24 +123,24 @@ def get_url(choice):
     """
 
     config = load_from_config()
-    if choice == "1":
-        return config["apiUrls"]["beaverBus"]
-    elif choice == "2":
-        return config["apiUrls"]["terms"]
-    elif choice == "3":
-        print(config["generalStrings"]["exiting"])
+    if choice == '1':
+        return config['apiUrls']['beaverBus']
+    elif choice == '2':
+        return config['apiUrls']['terms']
+    elif choice == '3':
+        print(config['generalStrings']['exiting'])
     else:
-        print(config["generalStrings"]["invalidChoice"])
+        print(config['generalStrings']['invalidChoice'])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """Driver function
     """
 
     config = load_from_config()
     access_token = get_access_token()
-    content_type = config["accessToken"]["headers"]["Content-Type"]
-    header = {"Content-Type": content_type, "Authorization": access_token}
+    content_type = config['accessToken']['headers']['Content-Type']
+    header = {'Content-Type': content_type, 'Authorization': access_token}
 
     while True:
         parameters = {
@@ -148,10 +148,10 @@ if __name__ == "__main__":
         show_tasks()
         user_choice = get_user_choice()
         api_url = get_url(user_choice)
-        if user_choice == "3":
+        if user_choice == '3':
             break
         if (
-            user_choice == "1"
-            or user_choice == "2"
+            user_choice == '1'
+            or user_choice == '2'
         ):
             api_call = get_api_data(api_url, parameters, header)
